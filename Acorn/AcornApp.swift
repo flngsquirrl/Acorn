ad55cf95ab8e7f4f9eed040e44fc4c5b31399c9a
flngsquirrl <y.charniak@gmail.com>
2025-02-11 21:57:32 +0100

    Init the repo

---
+++ Acorn/AcornApp.swift
@@ -0,0 +1,16 @@
+//
+// AcornApp.swift
+// Acorn
+//
+// Created by flngsquirrl on 11/02/2025. 
+
+import SwiftUI
+
+@main
+struct AcornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Acorn/ContentView.swift
@@ -0,0 +1,20 @@
+//
+// ContentView.swift
+// Acorn
+//
+// Created by flngsquirrl on 11/02/2025. 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Acorn!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
