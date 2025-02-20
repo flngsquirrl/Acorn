0843ade1b49393f18ce56419320d0f7c43860ec5
flngsquirrl <y.charniak@gmail.com>
2025-02-20 19:05:39 +0100

    Test all filter options are present in the view

--- AcornTests/Views/SegmentedFilterViewTests.swift
+++ AcornTests/Views/SegmentedFilterViewTests.swift
@@ -11,6 +11,8 @@ import ViewInspector
 
 @MainActor
 struct SegmentedFilterViewTests {
+    typealias InspectableFilterOptionView = InspectableView<ViewType.View<FilterOptionView>>
+
     @Test("Filter model toggle is called after an option is tapped",
           arguments: FilterOption.allCases)
     func toggleFilterModel_afterOptionTapped(option: FilterOption) async throws {
@@ -34,14 +36,42 @@ struct SegmentedFilterViewTests {
     }
 
     func tap(option: FilterOption, in view: some View) throws {
-        let optionText = try LocalizationTestUtils.getLocalizedString(for: option.rawValue)
+        let optionView = try findOptionView(option: option, in: view)
+        try optionView.callOnTapGesture()
+    }
+
+    func findOptionView(option: FilterOption, in view: some View) throws -> InspectableFilterOptionView {
+        let optionText = try getText(for: option)
 
         let optionViewOptional = try? view.inspect().find(FilterOptionView.self) {
-            let optionView = try? $0.find(text: optionText)
-            return optionView != nil
+            let textView = try? $0.find(text: optionText)
+            return textView != nil
         }
 
         let optionView = try #require(optionViewOptional)
-        try optionView.callOnTapGesture()
+        return optionView
+    }
+
+    func getText(for option: FilterOption) throws -> String {
+        try LocalizationTestUtils.getLocalizedString(for: option.rawValue)
+    }
+
+    @Test("All filter options are present in the view")
+    func allOptionsPresent() throws {
+        let model = try arrangeFilterModel()
+        let view = arrangeTestView(with: model)
+
+        let optionViews = try view.inspect().findAll(FilterOptionView.self)
+        #expect(optionViews.count == FilterOption.allCases.count)
+
+        let optionTexts = try FilterOption.allCases.map { try getText(for: $0) }
+        var optionTextsSet = Set(optionTexts)
+        #expect(optionTexts.count == optionViews.count)
+
+        for optionView in optionViews {
+            let text = try optionView.anyView().anyView().text().string()
+            optionTextsSet.remove(text)
+        }
+        #expect(optionTextsSet.isEmpty)
     }
 }
