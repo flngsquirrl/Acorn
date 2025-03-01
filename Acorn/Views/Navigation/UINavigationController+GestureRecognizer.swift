bf4e234778357647cf8838d344e4c438049147a3
flngsquirrl <y.charniak@gmail.com>
2025-03-01 14:52:47 +0100

    Fix navigating back on swipe
    When default Back button is hidden in navigation bar, popping top view from navigation stack stops functioning.
    To fix this, extend UINavigationController from UIKit.

---
+++ Acorn/Views/Navigation/UINavigationController+GestureRecognizer.swift
@@ -0,0 +1,22 @@
+//
+// UINavigationController+GestureRecognizer.swift
+// Acorn
+//
+// Created by flngsquirrl on 01/03/2025.
+
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+
+    public func gestureRecognizer(_: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith _: UIGestureRecognizer) -> Bool {
+        true
+    }
+}
